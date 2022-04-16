8630bfb32a2b0f3114bffc974ebc7fcae9cb72de
Moopz-Dev <pongzthor@gmail.com>
2022-04-16 07:33:59 +0700

    ✨ feat: adds HTML templates

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello Flask Framework</h1>"
+    return render_template("index.html")
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Me</h1>"
+    return render_template("about.html")
 
 
 @app.route('/admin')
 def admin():
-    return "<h1>Hello Admin</h1>"
+    return render_template("admin.html")
 
 
 @app.route('/user/<name>/<age>')
